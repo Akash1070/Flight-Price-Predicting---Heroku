326379c2dc792fa589d9c31603e4f09bef4c84db
Akash Kumar Jha <89967813+Akash1070@users.noreply.github.com>
2022-09-15 13:59:09 +0530

    Update build.py

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ import numpy as np
 import sklearn
 from sklearn.model_selection import train_test_split
 
-df=pd.read_csv('deploy_df')
+df=pd.read_excel('Data_Train.xlsx')
 
 df.drop('Unnamed: 0',axis=1,inplace=True)
 
